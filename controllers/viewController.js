357a759d2c97e7be5a36a2ee9ebf87a7115bd16c
Fic-1 <filip.brkovic1@gmail.com>
2023-11-20 11:15:42 +0100

    Created functionality that landing page shows products

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -7,6 +7,7 @@ const AppError = require('../utils/appError');
 exports.getLandingPage = catchAsync(async (req, res, next) => {
   //* 1) Get tour data from the collection
   const products = await Product.find();
+  //TODO: Napraviti funkcionalnost da se dobije limitiran broj rezultata za landing page (npr top-5)
   //* 2) Build template
 
   //* 3) Render template using the tour data from 1)
